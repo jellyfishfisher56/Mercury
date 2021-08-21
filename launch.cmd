ef02d31ee686bdaea71f6bc1edd88db5767216d2
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-21 11:47:14 +0100

    Create launch.cmd

---
+++ launch.cmd
@@ -0,0 +1,16 @@
+cmd launchfile cs f EXCEPT all EXCEPT app.exe
+ADD exe TO ms dos
+gui win
+linux ker ty ui 90 op
+0 o -6 -o
+-0 -9
+xt rt ak sd on cmd o cui out on gui in
+sc exe s
+rrun hj kl
+[p
+poi 90 df
+p00]
+56 gh
+int str hhygtg ;p0 
+90;
+sc da dd add dd cs cc
